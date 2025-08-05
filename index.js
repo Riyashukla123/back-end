7720895e30e5ff5b9a1c9e40bff8bb28f43f560e
Riyashukla123 <riya70077@gmail.com>
2025-08-05 21:56:00 +0530

    caros preflight fix

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
+app.options('*', cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
